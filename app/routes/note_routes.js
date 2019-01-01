ba7ee428f0bd3cf71531f62246bb5b82e8157a19
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2019-01-01 13:55:43 +0200

    routes

--- app/routes/note_routes.js
+++ app/routes/note_routes.js
@@ -3,7 +3,36 @@ const departments = groceristar.getDepartments();
 
 console.log( departments );
 
-module.exports = function(app, db) {
+// function getGroceryById( id ) {
+//   return groceristar.getGroceryById(id);
+// }
+
+// function getFullGrocery( name ) {
+//   return groceristar.getGroceryByNameWithDepAndIng(name);
+// }
+//
+// function getGroceryCollection(){
+//   return groceristar.getGroceryShowcase();
+// }
+
+// function getAllGrocery(){
+//   return groceristar.getAllGrocery();
+// }
+
+function getGroceryDataFromId(id){
+
+  console.log(getGroceryById(id));
+  let grocery     = getGroceryById(id)[0];
+  let groceryName = grocery.name;
+  let groceryWithDepAndIng = getFullGrocery(groceryName);
+  return {
+    'name': groceryName,
+    'items': groceryWithDepAndIng
+  };
+}
+
+
+const getNotes = function(app, db) {
 
   // app.post('/notes', (req, res) => {
   //   res.send('Hello')
@@ -15,3 +44,105 @@ module.exports = function(app, db) {
   });
 
 };
+
+//***
+const getGroceryById = function(app, db) {
+
+  app.get('/grocery/:id', (req, res) => {
+    // console.log(req.body)
+    const result = groceristar.getGroceryById(req.params.id);
+    res.send(result)
+  });
+
+};
+
+const getFullGrocery = function(app, db) {
+
+  app.get('/grocery-name/:name', (req, res) => {
+    // console.log(req.body)
+    const result = groceristar.getGroceryByNameWithDepAndIng(req.params.name);
+    res.send(result)
+  });
+
+};
+
+const getGroceryCollection = function(app, db) {
+
+  app.get('/grocery/collection', (req, res) => {
+    // console.log(req.body)
+
+    const result = groceristar.getGroceryShowcase()
+    res.send(result)
+  });
+
+};
+
+const getAllGrocery = function(app, db) {
+
+  app.get('/grocery/all', (req, res) => {
+    // console.log(req.body)
+    // groceristar.getAllGrocery();
+    const result = groceristar.getAllGrocery();
+    res.send(result)
+  });
+
+};
+
+const getGroceryDataFromId = function(app, db) {
+
+  app.get('/grocery/data/:id', (req, res) => {
+    // console.log(req.body)
+    res.send('Hello')
+    const result = groceristar.getGroceryDataFromId(req.params.id);
+    res.send(result)
+  });
+
+};
+// 
+// const getNotes = function(app, db) {
+//
+//   app.get('/notes', (req, res) => {
+//     // console.log(req.body)
+//     res.send('Hello')
+//   });
+//
+// };
+//
+// const getNotes = function(app, db) {
+//
+//   app.get('/notes', (req, res) => {
+//     // console.log(req.body)
+//     res.send('Hello')
+//   });
+//
+// };
+//
+// const getNotes = function(app, db) {
+//
+//   app.get('/notes', (req, res) => {
+//     // console.log(req.body)
+//     res.send('Hello')
+//   });
+//
+// };
+//
+// const getNotes = function(app, db) {
+//
+//   app.get('/notes', (req, res) => {
+//     // console.log(req.body)
+//     res.send('Hello')
+//   });
+//
+// };
+
+
+
+module.export {
+  getGroceryById,
+  getFullGrocery,
+  getGroceryDataFromId,
+  getGroceryCollection,
+  getAllGrocery,
+
+  getNotes
+}
