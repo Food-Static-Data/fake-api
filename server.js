f30fac111b0004d57a9baebbe7e2f0b140a40977
Vadim <vadim.putrov@gmail.com>
2019-03-08 12:54:09 +0200

    fix cors

--- server.js
+++ server.js
@@ -4,9 +4,15 @@ const bodyParser = require('body-parser')
 const app = express()
 const db = require('./app/config/db')
 
-var port = process.env.PORT || 3000
+var port = process.env.PORT || 4000
 
 app.use(bodyParser.urlencoded({ extended: true }))
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 require('./app/routes')(app, {})
 
