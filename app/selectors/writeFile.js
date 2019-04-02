ee3febddd07251fb1ba9f5e6f420007b317ea683
Vadim <vadim.putrov@gmail.com>
2019-04-02 20:21:00 +0300

    add writing in file

---
+++ app/selectors/writeFile.js
@@ -0,0 +1,21 @@
+const staticData = require('@groceristar/sd-wrapper')
+
+const fs = require("fs");
+const { promisify } = require("util");
+
+const writeFilePromisify = promisify(fs.writeFile);
+
+async function main() {
+    await writeFilePromisify("./app/selectors/users.json",
+    JSON.stringify(staticData.users));
+
+    console.info("file created successfully with promisify and async/await!");
+}
+
+function writeFile(){
+    main().catch(error => console.error(error));
+}
+
+module.exports = {
+    writeFile
+}
